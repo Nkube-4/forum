9e5c16c1a503ecf806711bdbeb63af27443134f7
Nkube-4 <benjamin.vm0@gmail.com>
2020-07-12 01:03:55 -0500

    chat fixes

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -5,18 +5,33 @@
    let chatList = document.querySelector(".chatList");
    let chatForm = document.querySelector(".chatForm");
    let chatInput = document.querySelector(".chatInput");
+   let loginForm = document.querySelector(".loginForm");
+   let userCount = document.querySelector(".userCount");
 
-   chatForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      socket.emit("chat message", chatInput.value);
-      chatInput.value = "";
-   });
+   if(chatForm) {
+      chatForm.addEventListener("submit", (e) => {
+         e.preventDefault();
+         socket.emit("chat message", chatInput.value);
+         chatInput.value = "";
+      });
+   }
+
+   if(loginForm) {
+      loginForm.addEventListener("submit", (e) => {
+         // console.log("login");
+         socket.emit("login");
+      });
+   }
 
    socket.on("new chat message", res => {
       console.log(res);
       chatList.insertAdjacentHTML("beforeend",`<li class="chatItem">${res.userName}: ${res.msg}</li>`);
    });
    
+   socket.on("users update", res => {
+      console.log(res);
+      userCount.innerText = `Online users: ${res.usersConnected}`;
+   });
 
 })();
 
--- socket.js
+++ socket.js
@@ -4,30 +4,44 @@ module.exports = function(io) {
 
    let connectedUsers = {};
    let anonCount = 0;
+   let usersConnected = 0;
    let userName = "";
 
    io.on("connection", (socket) => {
       console.log("user connected");
       console.log(socket.request.session);
+      usersConnected += 1;
+
+      io.emit("users update", {usersConnected});
 
       if(!socket.request.session.passport) {
          anonCount += 1;
-         userName = "Anon"+anonCount;
-         connectedUsers[userName] = socket;
+         socket.userName = "Anon"+anonCount;
+         connectedUsers[socket.userName] = socket;
       } else {
-         userName = socket.request.session.passport.user;
-         connectedUsers[userName] = socket;
+         socket.userName = socket.request.session.passport.user;
+         connectedUsers[socket.userName] = socket;
       }
 
+      socket.on("login", function() {
+         console.log(socket.request.session);
+         delete connectedUsers[socket.userName];
+         socket.userName = socket.request.session.passport;
+         connectedUsers[socket.userName] = socket;
+      });
+      
+
       socket.on("disconnect", function () {
-         delete connectedUsers[userName];
-         return console.log("The client has disconnected");
+         usersConnected -= 1;
+         io.emit("users update", { usersConnected });
+         delete connectedUsers[socket.userName];
+         return console.log("The client has disconnected" + socket.userName);
       });
       
       socket.on("chat message", function(msg) {
          console.log(socket.request.session);
          console.log(msg, userName);
-         io.emit("new chat message", {userName, msg})
+         io.emit("new chat message", {userName:socket.userName, msg})
       })
       console.log(connectedUsers);
       
